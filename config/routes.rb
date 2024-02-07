018b3e5588430ee76fe647168c456255a8170e53
Daisy Chew <150085367+daisychew@users.noreply.github.com>
2024-02-07 21:22:03 +0000

    finished movies_controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   get("/", { :controller => "misc", :action => "homepage" })
 
   get("/directors", controller: "movies", action: "directors")
+
+  get("/movies", controller: "movies", action: "movies")
+
+  get("/actors", controller: "movies", action: "actors")
 end
