dd4f14287802dceb55b5a11cb03dc1a6c09085a6
Daisy Chew <150085367+daisychew@users.noreply.github.com>
2024-02-08 18:06:19 +0000

    finished each individual acto's page until the movie details table

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -6,15 +6,18 @@ class ActorsController < ApplicationController
   end
   
   def dynamic_actors
-    actors = Actor.all
     @actor_id = params.fetch("actor_id")
     @actor_info = Actor.where({:id => @actor_id}).at(0)
+
     @actor_name = @actor_info.name
     @actor_dob = @actor_info.dob
     @actor_bio = @actor_info.bio
     @actor_image = @actor_info.image
     @actor_created_at = @actor_info.created_at
     @actor_updated_at = @actor_info.updated_at
+
+    @characters_played = Character.where({:actor_id => @actor_id})
+    
 
     render(template: "actor_templates/actors_show")
   end
