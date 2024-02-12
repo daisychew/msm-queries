a0ee297cde87f1053226c703950ac087762ebcb3
Daisy Chew <150085367+daisychew@users.noreply.github.com>
2024-02-12 22:40:29 +0000

    finisehd all - before rake grade

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -17,12 +17,19 @@ class ActorsController < ApplicationController
     @actor_updated_at = @actor_info.updated_at
 
     @characters_played = Character.where({:actor_id => @actor_id})
-    @characters_movies = []
-      (0...@characters_movies.length).each do |i|
-        @characters_movies.push(@characters_played.at(i)[movie_id])
-      end
+    @character_name = @characters_played.pluck(:name)
+    @character_movie_id = @characters_played.pluck(:movie_id)
 
-    @movie
+    @movie_info = Movie.where({:id => @character_movie_id}).at(0)
+  
+    @movie_title = @movie_info.title
+    @movie_duration = @movie_info.duration
+    @movie_description = @movie_info.description
+    @movie_image = @movie_info.image
+    @movie_year = @movie_info.year
+    @movie_created_at = @movie_info.created_at
+    @movie_updated_at = @movie_info.updated_at
+
     render(template: "actor_templates/actors_show")
   end
 end
