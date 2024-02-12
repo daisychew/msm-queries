5af01e4d34a97e67fe1d038ba184d79fc0b62e4e
Daisy Chew <150085367+daisychew@users.noreply.github.com>
2024-02-12 21:06:36 +0000

    finished all movies files

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -8,15 +8,16 @@ class MoviesController < ApplicationController
 
   def dynamic_movies
     @movie_id = params.fetch("movie_id")
-    @actor_info = Actor.where({:id => @actor_id}).at(0)
+    @movie_info = Movie.where({:id => @movie_id}).at(0)
 
-    @actor_name = @actor_info.name
-    @actor_dob = @actor_info.dob
-    @actor_bio = @actor_info.bio
-    @actor_image = @actor_info.image
-    @actor_created_at = @actor_info.created_at
-    @actor_updated_at = @actor_info.updated_at
+    @movie_title = @movie_info.title
+    @movie_duration = @movie_info.duration
+    @movie_description = @movie_info.description
+    @movie_image = @movie_info.image
+    @movie_year = @movie_info.year
+    @movie_created_at = @movie_info.created_at
+    @movie_updated_at = @movie_info.updated_at
 
-    render(template: "actor_templates/actors_show")
+    render(template: "movie_templates/movies_show")
   end
 end
